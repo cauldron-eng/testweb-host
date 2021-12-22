fed177f405fdb80357994c4daa129dd5ac4a37f0
Matthew Newcombe <a@mnewcombe.com>
2021-12-22 17:31:43 +0000

    Bump

--- Build/NightWebGL.framework.js
+++ Build/NightWebGL.framework.js
@@ -1295,7 +1295,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3629824;
+STATICTOP = STATIC_BASE + 3630544;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3361,7 +3361,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3629824;
+var STATIC_BUMP = 3630544;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
